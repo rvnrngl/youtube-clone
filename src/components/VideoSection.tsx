d3fe008d298e0e2817890cd5921fc569e691428a
Reevss <0217.raven.ringel@gmail.com>
2023-09-25 18:22:50 +0800

    added play on hover in videos

--- src/components/VideoSection.tsx
+++ src/components/VideoSection.tsx
@@ -1,19 +1,43 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import videoData from "../data/data.json";
 
 export const VideoSection: React.FC = () => {
   const videos = videoData.videos;
+  const videoRefs = useRef<Array<HTMLVideoElement | null>>(
+    new Array(videos.length).fill(null),
+  );
+
+  const handleEnter = (index: number) => {
+    const video = videoRefs.current[index];
+    if (video) {
+      video.play();
+    }
+  };
+  const handleLeave = (index: number) => {
+    const video = videoRefs.current[index];
+    if (video) {
+      video.pause();
+    }
+  };
+
+  // Use useEffect to update the videoRefs when the videos array changes
+  useEffect(() => {
+    videoRefs.current = videoRefs.current.slice(0, videos.length);
+  }, [videos]);
+
   return (
     <div className="grid h-full w-full grid-cols-1 gap-4 overflow-y-auto sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
       {videos.map((item, index) => {
         return (
           <div
             key={index}
+            onMouseEnter={() => handleEnter(index)}
+            onMouseLeave={() => handleLeave(index)}
             className="flex cursor-pointer flex-col gap-2 duration-300 ease-in-out hover:transition"
           >
             <video
+              ref={(video) => (videoRefs.current[index] = video)}
               src={item.videoUrl}
-              autoPlay={false}
               loop
               muted
               className="rounded-sm duration-300 hover:rounded-none hover:transition md:rounded-lg"
