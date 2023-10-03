041d152de1951899c9ce705bf6cc9f0b96d4343d
Reevss <0217.raven.ringel@gmail.com>
2023-10-03 15:55:46 +0800

    fix 404 not found when navigating to watchPage

--- src/components/VideosColumn.tsx
+++ src/components/VideosColumn.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { VideoProps } from "./VideoSection";
 import { BiDotsVerticalRounded } from "react-icons/bi";
 
@@ -7,12 +8,13 @@ type VideoColumnProps = {
 
 export const VideosColumn = (props: VideoColumnProps) => {
   const { videos } = props;
+
   return (
     <div className="flex w-full flex-col gap-2">
       {videos.map((video: VideoProps, index: number) => {
         return (
-          <a
-            href={`watch?id=${video.id}`}
+          <Link
+            to={`?id=${video.id}`}
             key={index}
             className="group flex w-full cursor-pointer flex-row items-start justify-between gap-2"
           >
@@ -23,7 +25,7 @@ export const VideosColumn = (props: VideoColumnProps) => {
                 className="aspect-video w-2/4 max-w-[200px] rounded-lg"
               />
               <div className="flex flex-col">
-                <span className="text-base font-bold leading-5">
+                <span className="line-clamp-1 text-base font-bold leading-5">
                   {video.title}
                 </span>
                 <p className="line-clamp-2 text-xs">{video.author}</p>
@@ -36,7 +38,7 @@ export const VideosColumn = (props: VideoColumnProps) => {
             <div className="invisible flex items-center justify-center group-hover:visible">
               <BiDotsVerticalRounded size={20} />
             </div>
-          </a>
+          </Link>
         );
       })}
     </div>
